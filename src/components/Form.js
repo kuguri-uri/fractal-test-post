1b2ceaa3f05182a6a5791b80c13203a9668865ed
Vera <mokeeva.vk@phystech.edu>
2023-08-27 20:59:27 +0300

    tryin' postdata

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,20 @@ import React from 'react';
 import  { useState } from 'react';
 
 const Form = () => {
-    const [inputValue, setInputValue] = useState('');
+    // const postData = {
+    //   uniquekey: formsStore.CalculatorData.uniquekey,
+    //   firstname: formsStore.CalculatorData.firstname,
+    //   email: formsStore.CalculatorData.email,
+    //   phone1: formsStore.CalculatorData.phone1,
+    //   fromzip: formsStore.CalculatorData.fromzip,
+    //   tozip: formsStore.CalculatorData.tozip,
+    //   movedate: formsStore.CalculatorData.movedate,
+    //   movesize: formsStore.CalculatorData.movesize,
+    //   remarks: JSON.stringify(formsStore.utmValues),
+    // };
     const [error, setError] = useState('');
+
+    const [inputValue, setInputValue] = useState('');
   
     const handleInputChange = (e) => {
       setInputValue(e.target.value);
@@ -18,6 +30,11 @@ const Form = () => {
         setError('Field is empty');
         return;
       }
+      // if (!postData.firstname.trim()) {
+      //   setError('Field is empty');
+      //   return;
+      // }
+
   
       try {
         const response = await fetch('https://api.network-leads.com/post', {
@@ -39,7 +56,7 @@ const Form = () => {
         <input
           className={`w-full border rounded px-3 py-2 ${error ? 'border-red-500' : ''}`}
           type="text"
-          placeholder="Enter something..."
+          placeholder="Name"
           value={inputValue}
           onChange={handleInputChange}
         />
